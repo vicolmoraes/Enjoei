a85f05c60efbc17c8412fd05557a146791bf325c
vitoria moraes <vicolmoraes@gmail.com>
2019-04-17 12:56:10 -0300

    Tela de Erro adicionada ao Manifest

--- app/src/main/java/com/enjoei/vicolmoraes/enjoei/View/ErroActivity.java
+++ app/src/main/java/com/enjoei/vicolmoraes/enjoei/View/ErroActivity.java
@@ -1,7 +1,10 @@
 package com.enjoei.vicolmoraes.enjoei.View;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.enjoei.vicolmoraes.enjoei.R;
 
@@ -10,5 +13,18 @@ public class ErroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.erro_activity);
+
+        setarBotao();
+    }
+
+    private void setarBotao() {
+        Button tentarNovamente = findViewById(R.id.bt_erro_tentar);
+        tentarNovamente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), IndexActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
