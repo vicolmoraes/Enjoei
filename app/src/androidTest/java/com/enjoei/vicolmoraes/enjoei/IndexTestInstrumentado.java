4b04fdd9cc97ddbeec70a1125989586ae548ca81
vitoria moraes <vicolmoraes@gmail.com>
2019-04-18 13:58:03 -0300

    melhora no teste do recyclerView

--- app/src/androidTest/java/com/enjoei/vicolmoraes/enjoei/IndexTestInstrumentado.java
+++ app/src/androidTest/java/com/enjoei/vicolmoraes/enjoei/IndexTestInstrumentado.java
@@ -5,13 +5,10 @@ import android.content.Intent;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.v7.widget.RecyclerView;
-import android.widget.TextView;
 
 import com.enjoei.vicolmoraes.enjoei.Model.ProdutoVO;
 import com.enjoei.vicolmoraes.enjoei.View.IndexActivity;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -23,8 +20,10 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -49,6 +48,7 @@ public class IndexTestInstrumentado {
     public void abreHome_verificaExibicaoProduto() {
 
         synchronized (mActivityRule) {
+
             //Prepara
             Intent intent = new Intent();
             mActivityRule.launchActivity(intent);
@@ -59,16 +59,27 @@ public class IndexTestInstrumentado {
             } catch (Exception c) {
 
             }
-
-            RecyclerView re = mActivityRule.getActivity().findViewById(R.id.rv_home_produtos);
-            TextView textoTitulo = re.getChildAt(0).findViewById(R.id.tv_produto_item_titulo);
-            TextView valor = re.getChildAt(0).findViewById(R.id.tv_produto_item_valor);
-            TextView likes = re.getChildAt(0).findViewById(R.id.tv_produto_icone_like);
-
             //Verifica
-            Assert.assertEquals(NumberFormat.getCurrencyInstance().format(produtoVO.getPrice()), valor.getText().toString());
-            Assert.assertEquals(produtoVO.getTitle(), textoTitulo.getText().toString());
-            Assert.assertEquals(String.valueOf(produtoVO.getLikes_count()), likes.getText().toString());
+            onView(withId(R.id.rv_home_produtos))
+                    .check(matches(hasDescendant(
+                            allOf(
+                                    withId(R.id.tv_produto_item_valor),
+                                    withText(NumberFormat.getCurrencyInstance().format(produtoVO.getPrice()))
+                            ))));
+
+            onView(withId(R.id.rv_home_produtos))
+                    .check(matches(hasDescendant(
+                            allOf(
+                                    withId(R.id.tv_produto_item_titulo),
+                                    withText(produtoVO.getTitle())
+                            ))));
+
+            onView(withId(R.id.rv_home_produtos))
+                    .check(matches(hasDescendant(
+                            allOf(
+                                    withId(R.id.tv_produto_icone_like),
+                                    withText(String.valueOf(produtoVO.getLikes_count()))
+                            ))));
         }
     }
 
@@ -86,6 +97,7 @@ public class IndexTestInstrumentado {
             } catch (Exception c) {
 
             }
+            //Executa
             onView(withId(R.id.rv_home_produtos))
                     .perform(actionOnItemAtPosition(0, click()));
             try {
@@ -95,7 +107,6 @@ public class IndexTestInstrumentado {
 
             }
             //Verifica
-
             onView(withId(R.id.tv_produto_titulo)).check(matches(withText(produtoVO.getTitle())));
             onView(withId(R.id.tv_produto_descricao)).check(matches(withText(produtoVO.getContent())));
             onView(withId(R.id.tv_produto_valor_atual)).check(matches(withText(NumberFormat.getCurrencyInstance().format(produtoVO.getPrice()))));
